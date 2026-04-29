b4d4ff4cb561c20c1c1d78a8cc59f0e509150099
Mai Van Minh <maivanminh.se@gmail.com>
2026-04-29 18:09:23 +0700

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int min = 1;
+        int max = 1;
+        for (int pile: piles) {
+            max = Math.max(max, pile);
+        }
+
+        int mid = 1;
+        while (min < max) {
+            mid = min + (max - min) / 2;
+            int hours = 0;
+            for (int pile: piles) {
+                hours += Math.ceil((double) pile / mid);
+            }
+
+            if (hours > h) {
+                min = mid + 1;
+            }   else max = mid;
+        }
+
+        return min;
+    }
+}
